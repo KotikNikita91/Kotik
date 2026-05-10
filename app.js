98301341efb3d3d572cfb15e547bf9c875976622
KotikNikita91 <Vzksm.kotik@gmail.com>
2026-05-10 20:48:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Конфигурация
 const CONFIG = {
-  scriptURL: 'https://script.google.com/macros/s/AKfycbxNFAEsdDx9M86ud1hMC4A2XMPNNPqz5rLmO3LSbnfH9BjHidAdaU-akcUJfpwFE31tdw/exec',
+  scriptURL: 'https://script.google.com/macros/s/AKfycbzJklOHc6K5_p3TAI_DaWyUy3dwL7jOvm-utvpuM8WC6HwqUUgLO_lhLbA1X9KkiwFLYA/exec',
   maxMobileWidth: 500,
   storageKeys: {
     city: 'family_budget_city',
